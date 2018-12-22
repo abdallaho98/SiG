ffc44fc264bbf94a276db982a492161bdf316de6
Herzallah Abdallah <32783064+abdallaho98@users.noreply.github.com>
2018-12-22 14:02:28 +0100

    setup

---
+++ SiG/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿using System;
+
+namespace SiG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.appManager1 = new DotSpatial.Controls.AppManager();
+            this.spatialDockManager1 = new DotSpatial.Controls.SpatialDockManager();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.legend1 = new DotSpatial.Controls.Legend();
+            this.map1 = new DotSpatial.Controls.Map();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Edit = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openRasterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.pointToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polyLineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polyGoneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSplitButton2 = new System.Windows.Forms.ToolStripSplitButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.spatialDockManager1)).BeginInit();
+            this.spatialDockManager1.Panel1.SuspendLayout();
+            this.spatialDockManager1.Panel2.SuspendLayout();
+            this.spatialDockManager1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // appManager1
+            // 
+            this.appManager1.Directories = ((System.Collections.Generic.List<string>)(resources.GetObject("appManager1.Directories")));
+            this.appManager1.DockManager = this.spatialDockManager1;
+            this.appManager1.HeaderControl = null;
+            this.appManager1.Legend = this.legend1;
+            this.appManager1.Map = this.map1;
+            this.appManager1.ProgressHandler = null;
+            this.appManager1.ShowExtensionsDialogMode = DotSpatial.Controls.ShowExtensionsDialogMode.Default;
+            // 
+            // spatialDockManager1
+            // 
+            this.spatialDockManager1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spatialDockManager1.Location = new System.Drawing.Point(0, 27);
+            this.spatialDockManager1.Name = "spatialDockManager1";
+            // 
+            // spatialDockManager1.Panel1
+            // 
+            this.spatialDockManager1.Panel1.Controls.Add(this.tabControl1);
+            // 
+            // spatialDockManager1.Panel2
+            // 
+            this.spatialDockManager1.Panel2.Controls.Add(this.map1);
+            this.spatialDockManager1.Size = new System.Drawing.Size(1152, 613);
+            this.spatialDockManager1.SplitterDistance = 383;
+            this.spatialDockManager1.TabControl1 = this.tabControl1;
+            this.spatialDockManager1.TabControl2 = null;
+            this.spatialDockManager1.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(383, 613);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.legend1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(375, 584);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Legend";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // legend1
+            // 
+            this.legend1.BackColor = System.Drawing.Color.White;
+            this.legend1.ControlRectangle = new System.Drawing.Rectangle(0, 0, 369, 578);
+            this.legend1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.legend1.DocumentRectangle = new System.Drawing.Rectangle(0, 0, 187, 428);
+            this.legend1.HorizontalScrollEnabled = true;
+            this.legend1.Indentation = 30;
+            this.legend1.IsInitialized = false;
+            this.legend1.Location = new System.Drawing.Point(3, 3);
+            this.legend1.MinimumSize = new System.Drawing.Size(5, 5);
+            this.legend1.Name = "legend1";
+            this.legend1.ProgressHandler = null;
+            this.legend1.ResetOnResize = false;
+            this.legend1.SelectionFontColor = System.Drawing.Color.Black;
+            this.legend1.SelectionHighlight = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(238)))), ((int)(((byte)(252)))));
+            this.legend1.Size = new System.Drawing.Size(369, 578);
+            this.legend1.TabIndex = 0;
+            this.legend1.Text = "legend1";
+            this.legend1.VerticalScrollEnabled = true;
+            // 
+            // map1
+            // 
+            this.map1.AllowDrop = true;
+            this.map1.BackColor = System.Drawing.Color.White;
+            this.map1.CollectAfterDraw = false;
+            this.map1.CollisionDetection = false;
+            this.map1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map1.ExtendBuffer = false;
+            this.map1.FunctionMode = DotSpatial.Controls.FunctionMode.None;
+            this.map1.IsBusy = false;
+            this.map1.IsZoomedToMaxExtent = false;
+            this.map1.Legend = this.legend1;
+            this.map1.Location = new System.Drawing.Point(0, 0);
+            this.map1.Name = "map1";
+            this.map1.ProgressHandler = null;
+            this.map1.ProjectionModeDefine = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.ProjectionModeReproject = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.RedrawLayersWhileResizing = false;
+            this.map1.SelectionEnabled = true;
+            this.map1.Size = new System.Drawing.Size(765, 613);
+            this.map1.TabIndex = 0;
+            this.map1.ZoomOutFartherThanMaxExtent = false;
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(51, 24);
+            this.toolStripSplitButton1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // Edit
+            // 
+            this.Edit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openRasterToolStripMenuItem});
+            this.Edit.Image = ((System.Drawing.Image)(resources.GetObject("Edit.Image")));
+            this.Edit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(49, 24);
+            this.Edit.Text = "Edit";
+            // 
+            // openRasterToolStripMenuItem
+            // 
+            this.openRasterToolStripMenuItem.Name = "openRasterToolStripMenuItem";
+            this.openRasterToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.openRasterToolStripMenuItem.Text = "Open Raster";
+            this.openRasterToolStripMenuItem.Click += new System.EventHandler(this.openRasterToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1,
+            this.Edit,
+            this.toolStripButton1,
+            this.toolStripSplitButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1152, 27);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pointToolStripMenuItem,
+            this.polyLineToolStripMenuItem,
+            this.polyGoneToolStripMenuItem});
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(39, 24);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // pointToolStripMenuItem
+            // 
+            this.pointToolStripMenuItem.Name = "pointToolStripMenuItem";
+            this.pointToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.pointToolStripMenuItem.Text = "Point";
+            this.pointToolStripMenuItem.Click += new System.EventHandler(this.pointToolStripMenuItem_Click);
+            // 
+            // polyLineToolStripMenuItem
+            // 
+            this.polyLineToolStripMenuItem.Name = "polyLineToolStripMenuItem";
+            this.polyLineToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.polyLineToolStripMenuItem.Text = "PolyLine";
+            this.polyLineToolStripMenuItem.Click += new System.EventHandler(this.polyLineToolStripMenuItem_Click);
+            // 
+            // polyGoneToolStripMenuItem
+            // 
+            this.polyGoneToolStripMenuItem.Name = "polyGoneToolStripMenuItem";
+            this.polyGoneToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.polyGoneToolStripMenuItem.Text = "PolyGone";
+            this.polyGoneToolStripMenuItem.Click += new System.EventHandler(this.polyGoneToolStripMenuItem_Click);
+            // 
+            // toolStripSplitButton2
+            // 
+            this.toolStripSplitButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton2.Image")));
+            this.toolStripSplitButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton2.Name = "toolStripSplitButton2";
+            this.toolStripSplitButton2.Size = new System.Drawing.Size(39, 24);
+            this.toolStripSplitButton2.Text = "toolStripSplitButton2";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(0, 0);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(200, 100);
+            this.tabPage2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 640);
+            this.Controls.Add(this.spatialDockManager1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.spatialDockManager1.Panel1.ResumeLayout(false);
+            this.spatialDockManager1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spatialDockManager1)).EndInit();
+            this.spatialDockManager1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void map1_zoom(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private DotSpatial.Controls.AppManager appManager1;
+        private DotSpatial.Controls.SpatialDockManager spatialDockManager1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private DotSpatial.Controls.Legend legend1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private DotSpatial.Controls.Map map1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton Edit;
+        private System.Windows.Forms.ToolStripMenuItem openRasterToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripButton1;
+        private System.Windows.Forms.ToolStripMenuItem pointToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polyLineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polyGoneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton2;
+    }
+}
+
---
+++ SiG/Form1.cs
@@ -0,0 +1,151 @@
+﻿using DotSpatial.Controls;
+using DotSpatial.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DotSpatial.Symbology;
+using DotSpatial.Topology;
+
+
+
+namespace SiG
+{
+    public partial class Form1 : Form
+    {
+        private bool point = false, polyline = false, polygone = false;
+        private FeatureSet _markers,_gones;
+        private MapPointLayer _markerLayer;
+        private LinearRing linearRing;
+        private MapPolygonLayer _goneLayer;
+        private List<Coordinate> Corr;
+        private DotSpatial.Topology.Polygon mPolygone; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            appManager1.LoadExtensions();
+        }
+
+        private void openRasterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            map1.AddImageLayer();
+            map1.FunctionMode = DotSpatial.Controls.FunctionMode.Pan;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+
+        private void map1_Load(object sender, MouseEventArgs e)
+        {
+           
+            // Intercept only the right click for adding markers
+            if (((MouseEventArgs)e).Button != MouseButtons.Left) return;
+            if (point)
+            {
+                // Get the geographic location that was clicked
+                Coordinate c = map1.PixelToProj(new System.Drawing.Point(e.X, e.Y));
+                
+                // Add the new coordinate as a "point" to the point featureset
+                _markers.AddFeature(new DotSpatial.Topology.Point(c));
+
+                // Drawing will take place from a bitmap buffer, so if data is updated,
+                // we need to tell the map to refresh the buffer 
+                map1.MapFrame.Invalidate();
+            }
+            else if (polyline) {
+            } else {
+                // Get the geographic location that was clicked
+                Coordinate c = map1.PixelToProj(new System.Drawing.Point(e.X, e.Y));
+                
+
+                if (Corr.Count == 0)
+                {
+                    linearRing = new LinearRing(Corr);
+                    linearRing.Coordinates.Add(c);
+                    mPolygone = new DotSpatial.Topology.Polygon(linearRing);
+                }
+                else {
+                    linearRing.Coordinates.Add(c);
+                    _gones.AddFeature(mPolygone as IGeometry);
+                    // Drawing will take place from a bitmap buffer, so if data is updated,
+                    // we need to tell the map to refresh the buffer 
+                    map1.MapFrame.Invalidate();
+                }
+              
+               
+            }
+           
+        }
+
+        private void pointToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            point = true;
+            polygone = false;
+            polyline = false;
+            // Enable left click panning and mouse wheel zooming
+            map1.FunctionMode = FunctionMode.Pan;
+
+            // Handle mouse up event on the map
+            map1.MouseDoubleClick += map1_Load;
+
+            // The FeatureSet starts with no data; be sure to set it to the point featuretype
+            _markers = new FeatureSet(FeatureType.Point);
+
+            // The MapPointLayer controls the drawing of the marker features
+            _markerLayer = new MapPointLayer(_markers);
+
+            // The Symbolizer controls what the points look like
+            _markerLayer.Symbolizer = new PointSymbolizer(Color.Black, DotSpatial.Symbology.PointShape.Ellipse, 10);
+
+            _markerLayer.LegendText = "Points";
+            // A drawing layer draws on top of data layers, but is still georeferenced.
+            map1.Layers.Add(_markerLayer);
+        }
+
+        private void polyLineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            point = false;
+            polygone = false;
+            polyline = true;
+        }
+
+        private void polyGoneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            point = false;
+            polygone = true;
+            polyline = false;
+            // Enable left click panning and mouse wheel zooming
+            map1.FunctionMode = FunctionMode.Pan;
+
+            // Handle mouse up event on the map
+            map1.MouseDoubleClick += map1_Load;
+
+            // The FeatureSet starts with no data; be sure to set it to the polygone featuretype
+            _gones = new FeatureSet(FeatureType.Polygon);
+
+            // The MapPolygone controls the drawing of the marker features
+            _goneLayer = new MapPolygonLayer(_gones);
+
+            Corr = new List<Coordinate>();
+
+            // The Symbolizer controls what the points look like
+            _goneLayer.Symbolizer = new PolygonSymbolizer(Color.Blue);
+
+            _goneLayer.LegendText = "Surface";
+            // A drawing layer draws on top of data layers, but is still georeferenced.
+            map1.Layers.Add(_goneLayer);
+        }
+    }
+
+    
+}
